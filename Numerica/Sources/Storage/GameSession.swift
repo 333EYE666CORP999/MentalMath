5bc5d81f64a1669fd506cd5d88467a7832236dd7
Dmitry <dmi.aksyonov@gmail.com>
2024-09-11 13:23:57 +0300

    added problem persistent model

--- Numerica/Sources/Storage/GameSession.swift
+++ Numerica/Sources/Storage/GameSession.swift
@@ -15,6 +15,9 @@ final class GameSession {
     var goodAnswersCount: Int
     var badAnswersCount: Int
 
+    @Relationship(deleteRule: .noAction)
+    var problems = [Problem]()
+
     init(
         sessionDate: Date = Date(),
         goodAnswersCount: Int = .zero,
@@ -23,5 +26,17 @@ final class GameSession {
         self.sessionDate = sessionDate
         self.goodAnswersCount = goodAnswersCount
         self.badAnswersCount = badAnswersCount
+    }
+}
+
+@Model
+final class Problem {
+
+    var problem: String
+    var solution: Int
+
+    init(problem: String, solution: Int) {
+        self.problem = problem
+        self.solution = solution
     }
 }
